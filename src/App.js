9f3a20a6411e515adc632cab5a8b3001a5132e8f
thelycanthrope <69922095+thelycanthrope@users.noreply.github.com>
2021-10-10 20:27:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import AgeCalci from './AgeCalci';
+
 import AgeCalculator from './AgeCalculator';
 import './App.css';
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       
-     {/* <AgeCalci/> */}
+
      <AgeCalculator/>
     </div>
   );
